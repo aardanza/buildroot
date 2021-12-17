15e372d60b356fed78fc638164fbc2f153e8976e
aardanza <aardanza@indaba.es>
2021-12-17 14:06:58 +0100

    added u-boot compilation

--- board/amlogic/s905x3/post-build.sh
+++ board/amlogic/s905x3/post-build.sh
@@ -1,12 +1,29 @@
 #!/bin/sh
 
+#BOARD_DIR="$(dirname $0)"
+#MKIMAGE=$HOST_DIR/bin/mkimage
+
+#$MKIMAGE -C none -A arm64 -T script -d $BOARD_DIR/boot.txt $BINARIES_DIR/boot.scr
+
+# vendor u-boot uses uImage
+#if [ -e $BINARIES_DIR/Image ]; then
+#    $MKIMAGE -A arm64 -O linux -T kernel -C none -a 0x1080000 -e 0x1080000 \
+#	     -n linux -d $BINARIES_DIR/Image $BINARIES_DIR/uImage
+#fi
+
+#!/bin/sh
+
 BOARD_DIR="$(dirname $0)"
 MKIMAGE=$HOST_DIR/bin/mkimage
 
+# for Amlogic U-Boot
+# for SD card
+echo "INFO: SD CARD aml_autoscript"
 $MKIMAGE -C none -A arm64 -T script -d $BOARD_DIR/boot.txt $BINARIES_DIR/boot.scr
 
-# vendor u-boot uses uImage
-if [ -e $BINARIES_DIR/Image ]; then
-    $MKIMAGE -A arm64 -O linux -T kernel -C none -a 0x1080000 -e 0x1080000 \
-	     -n linux -d $BINARIES_DIR/Image $BINARIES_DIR/uImage
-fi
+#$MKIMAGE -C none -A arm64 -T script -d $BOARD_DIR/boot.txt $BINARIES_DIR/aml_autoscript
+
+
+# move dtb in amlogic subdir
+# mkdir ${BINARIES_DIR}/amlogic
+# mv ${BINARIES_DIR}/"meson-gxbb-nexbox-a95x.dtb" ${BINARIES_DIR}/amlogic
--- board/amlogic/s905x3/post-image.sh
+++ board/amlogic/s905x3/post-image.sh
@@ -4,9 +4,13 @@ BOARD_DIR="$(dirname $0)"
 GENIMAGE_CFG="${BOARD_DIR}/genimage.cfg"
 GENIMAGE_TMP="${BUILD_DIR}/genimage.tmp"
 
+# set -x
+
 rm -rf "${GENIMAGE_TMP}"
 
-cp "${BOARD_DIR}/sm1_s905x3_4g_1gbit.dtb" "${BINARIES_DIR}"
+#cp "${BOARD_DIR}/meson-sm1-x96-max-plus.dtb" "${BINARIES_DIR}"
+
+# generate image
 
 genimage                               \
 	--rootpath "${TARGET_DIR}"     \
@@ -14,3 +18,97 @@ genimage                               \
 	--inputpath "${BINARIES_DIR}"  \
 	--outputpath "${BINARIES_DIR}" \
 	--config "${GENIMAGE_CFG}"
+
+
+# merge bootloaders
+
+AML_TOOLS_DIR=utils/amlogic
+AML_BIN_DIR=${AML_TOOLS_DIR}/u-boot
+
+OUTPUT_BIN_DIR=${BINARIES_DIR}/tmp
+EMMC_BIN_DIR=${BINARIES_DIR}/emmc
+
+mkdir ${OUTPUT_BIN_DIR}
+rm ${OUTPUT_BIN_DIR}/*
+
+set -e
+
+##########################################
+# 	Mainline U-Boot (build by Buildroot)
+##########################################
+	echo "========================================================================"
+	echo "INFO: U-Boot used: <mainline> (build by Buildroot)"
+	echo "========================================================================"
+
+	cp ${AML_BIN_DIR}/gxl/bl2.bin   ${OUTPUT_BIN_DIR}/;
+	cp ${AML_BIN_DIR}/gxl/acs.bin   ${OUTPUT_BIN_DIR}/;
+	cp ${AML_BIN_DIR}/gxl/bl21.bin  ${OUTPUT_BIN_DIR}/;
+	cp ${AML_BIN_DIR}/gxl/bl30.bin  ${OUTPUT_BIN_DIR}/;
+	cp ${AML_BIN_DIR}/gxl/bl301.bin ${OUTPUT_BIN_DIR}/;
+	cp ${AML_BIN_DIR}/gxl/bl31.img  ${OUTPUT_BIN_DIR}/;
+	cp ${BINARIES_DIR}/u-boot.bin   ${OUTPUT_BIN_DIR}/bl33.bin
+
+	${AML_BIN_DIR}/blx_fix.sh						\
+				${OUTPUT_BIN_DIR}/bl30.bin 			\
+				${OUTPUT_BIN_DIR}/zero_tmp 			\
+				${OUTPUT_BIN_DIR}/bl30_zero.bin 		\
+				${OUTPUT_BIN_DIR}/bl301.bin 			\
+				${OUTPUT_BIN_DIR}/bl301_zero.bin 		\
+				${OUTPUT_BIN_DIR}/bl30_new.bin			\
+				bl30
+
+	python ${AML_BIN_DIR}/acs_tool.pyc 					\
+				${OUTPUT_BIN_DIR}/bl2.bin 				\
+				${OUTPUT_BIN_DIR}/bl2_acs.bin 			\
+				${OUTPUT_BIN_DIR}/acs.bin 				\
+				0
+
+	${AML_BIN_DIR}/blx_fix.sh 						\
+				${OUTPUT_BIN_DIR}/bl2_acs.bin 			\
+				${OUTPUT_BIN_DIR}/zero_tmp 			\
+				${OUTPUT_BIN_DIR}/bl2_zero.bin 			\
+				${OUTPUT_BIN_DIR}/bl21.bin 			\
+				${OUTPUT_BIN_DIR}/bl21_zero.bin 		\
+				${OUTPUT_BIN_DIR}/bl2_new.bin 			\
+				bl2
+
+	# encrypt bootloader
+
+	${AML_BIN_DIR}/gxl/aml_encrypt_gxl					\
+				--bl3enc					\
+				--input ${OUTPUT_BIN_DIR}/bl30_new.bin
+
+	${AML_BIN_DIR}/gxl/aml_encrypt_gxl					\
+				--bl3enc 					\
+				--input ${OUTPUT_BIN_DIR}/bl31.img
+
+	${AML_BIN_DIR}/gxl/aml_encrypt_gxl					\
+				--bl3enc 					\
+				--input ${OUTPUT_BIN_DIR}/bl33.bin
+
+	${AML_BIN_DIR}/gxl/aml_encrypt_gxl					\
+				--bl2sig 					\
+				--input ${OUTPUT_BIN_DIR}/bl2_new.bin 		\
+				--output ${OUTPUT_BIN_DIR}/bl2.n.bin.sig
+
+	${AML_BIN_DIR}/gxl/aml_encrypt_gxl					\
+				--bootmk 					\
+				--output ${OUTPUT_BIN_DIR}/u-boot.bin 		\
+				--bl2    ${OUTPUT_BIN_DIR}/bl2.n.bin.sig 	\
+				--bl30   ${OUTPUT_BIN_DIR}/bl30_new.bin.enc 	\
+				--bl31   ${OUTPUT_BIN_DIR}/bl31.img.enc		\
+				--bl33   ${OUTPUT_BIN_DIR}/bl33.bin.enc
+
+	cp ${OUTPUT_BIN_DIR}/u-boot.bin.* ${BINARIES_DIR}/
+
+
+	# Generating SDCARD.IMG
+
+	echo "Adding bootloader to SD card image..."
+
+	dd if=${BINARIES_DIR}/u-boot.bin.sd.bin of="${BINARIES_DIR}/sdcard.img" conv=fsync,notrunc bs=1 count=442 status=progress
+	dd if=${BINARIES_DIR}/u-boot.bin.sd.bin of="${BINARIES_DIR}/sdcard.img" conv=fsync,notrunc bs=512 skip=1 seek=1 status=progress
+
+	echo "========================================================================"
+	echo "	Done. Please run $ ./utils/flash-sdcard to flash image "
+	echo "========================================================================"
